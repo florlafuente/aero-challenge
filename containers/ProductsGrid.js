124719200caca780a990a8872e599553686f9b9d
florlafuente <lafuente.florencia@gmail.com>
2017-11-17 01:00:48 -0300

    Adds padding in grid

--- containers/ProductsGrid.js
+++ containers/ProductsGrid.js
@@ -4,13 +4,18 @@ import ProductCard from '../components/ProductCard'
 const ProductsGrid = () => (
   <section className='products-grid'>
     <Menu filter={true}/>
-    <ProductCard canRedeem={false}/>
+      <div className='product-cards-containers'>
+        <ProductCard canRedeem={false}/>
+      </div>
     <Menu filter={false} />
     <style jsx>{`
       .products-grid {
         height: 100%;
         padding: 65px 132px 74px;
         width: 100%;
+      }
+      .product-cards-containers {
+        padding: 42px 0 60px;
       }
     `}</style>
   </section>
