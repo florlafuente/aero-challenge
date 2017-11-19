6c8719f5f7ffc60b1b09ab6e7a33e1a7191357dd
florlafuente <lafuente.florencia@gmail.com>
2017-11-18 22:04:54 -0300

    Returning function

--- api/redeemProduct.js
+++ api/redeemProduct.js
@@ -1,15 +1,17 @@
 import fetch from 'isomorphic-unfetch'
 
 export function redeemProduct(id) {
-  return fetch('https://aerolab-challenge.now.sh/redeem', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTBjZjI3ZWU0OTYwMDAwNjBkMDBhYzQiLCJpYXQiOjE1MTA3OTc5NTB9.E1yF6dRIx3QNPbexT7ujlWFG2pE1tj7pQCNXmPE8NyQ'
-    },
-    body: {
+  return function (id) {
+    fetch('https://aerolab-challenge.now.sh/redeem', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTBjZjI3ZWU0OTYwMDAwNjBkMDBhYzQiLCJpYXQiOjE1MTA3OTc5NTB9.E1yF6dRIx3QNPbexT7ujlWFG2pE1tj7pQCNXmPE8NyQ'
+      },
+      body: {
       'productId': id 
-    }
-  })
+      }
+    })
+  }
 }
